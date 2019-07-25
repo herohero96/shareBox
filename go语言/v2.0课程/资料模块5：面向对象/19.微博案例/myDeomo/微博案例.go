0c6401a89a66e59d747fa7aa3d2d0d5c27483de4
liuzhiyong <1351493417@qq.com>
2019-07-25 08:35:46 +0800

    'tst'

--- go语言/v2.0课程/资料模块5：面向对象/19.微博案例/myDeomo/微博案例.go
+++ go语言/v2.0课程/资料模块5：面向对象/19.微博案例/myDeomo/微博案例.go
@@ -4,5 +4,22 @@ func main()  {
 
 
 
+}
+
+
+type Blogger struct {
+	Base
+	WeiBos []*PostContent
+	Comments map[int][]*PostContent
+	Fans []FansInterfacer
+}
+
+func NewBlogger(name string) *Blogger  {
+	blg := new(Blogger)
+	blg.Name = name
+	blog.Comments = make(map[int][]*PostContent)
+	blg.WeiBos = make([]*PostContent, 0)
+	return blg
 
 }
+
